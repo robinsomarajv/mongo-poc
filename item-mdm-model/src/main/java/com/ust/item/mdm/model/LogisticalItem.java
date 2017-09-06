b640856a3d9c443cba339d1474b4fde415a38616
U17078 <U17078@D451LTRV.ustr.com>
2017-09-06 12:51:49 +0530

    Litem load

--- item-mdm-model/src/main/java/com/ust/item/mdm/model/LogisticalItem.java
+++ item-mdm-model/src/main/java/com/ust/item/mdm/model/LogisticalItem.java
@@ -3,33 +3,473 @@ package com.ust.item.mdm.model;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
-import org.springframework.data.annotation.Id;
 
 public class LogisticalItem implements Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-	@Id
+	
+	private final static long serialVersionUID = -999590161740494050L;
+	
 	private String _id;
-
+	private String warehouseMinOrderQty;
+	private String isCannedOrderInd;
+	private String itemNbr;
+	private String baseDivisionCode;
+	private String warehouseMinLifeRemainingToReceiveQty;
+	private String omitTraitNbr;
+	private String isCancelWhenOutInd;
+	private String supplierLeadTimeQty;
+	private String isOfferedForSaleInd;
+	private String warehouseAlignmentCode;
+	private String seasonCode;
+	private String factoryId;
+	private String publishedGtin;
+	private String seasonYearNbr;
+	private String warehousePackGtin;
+	private String itemChangeSendWalmartWeekNbr;
+	private String supplierLeadTimeUomCode;
+	private String hasRfidInd;
+	private String shelfLabel3Description;
+	private String isImportInd;
+	private String orderablePackGtin;
+	private String warehouseAreaCode;
+	private String supplyItemSecondaryDescription;
+	private String reserveMerchandiseTypeCode;
+	private String isShelfLabelRequiredInd;
+	private String merchandiseProgramId;
+	private String hasSecurityTagInd;
+	private String supplyItemChangeReasonCode;
+	private String allowedTimeInWarehouseUomCode;
+	private String warehouseMaxOrderQty;
+	private List<String> destinationFormatCode = null;
+	private String warehousePackQtyUomCode;
+	private String legacyProductNbr;
+	private String allowedTimeInWarehouseQty;
+	private String isReplenishedByUnitInd;
+	private String mbmTypeCode;
+	private String supplierId;
+	private String isCorporateReplenishableInd;
+	private String isEcommerceReplenishableInd;
+	private String warehousePackCalcMethodCode;
+	private String recipientGln;
+	private String warehousePackCostAmt;
+	private String deptNbr;
+	private String supplyItemPrimaryDescription;
+	private String buyingRegionCode;
+	private Date supplyItemEffectiveDate;
+	private List<String> originCountryCode = null;
+	private String supplyItemStatusCode;
+	private String informationProviderId;
 	private String consumableGtin;
+	private String supplierStockId;
+	private String baseRetailUomCode;
+	private String isVariableWeightInd;
+	private String orderablePackCostAmt;
+	private String replenishSubTypeCode;
+	private String warehousePackQty;
+	private String informationProviderTypeCode;
+	private String itemTypeCode;
+	private String warehouseRotationTypeCode;
+	private String orderablePackQty;
+	private String targetMarketCode;
+	private Date sendStoreDate;
+	private String hasRigidPlasticPackagingContainerInd;
+	private String unitCostAmt;
+	private String baseRetailAmt;
+	private String finelineNbr;
+	private String isBackroomScaleInd;
+	private Date supplyItemExpireDate;
 
-	private String supplierGtin;
+	public String getWarehouseMinOrderQty() {
+		return warehouseMinOrderQty;
+	}
 
-	private String consumableGtinId;
+	public void setWarehouseMinOrderQty(String warehouseMinOrderQty) {
+		this.warehouseMinOrderQty = warehouseMinOrderQty;
+	}
 
-	private String supplierGtinId;
+	public String getIsCannedOrderInd() {
+		return isCannedOrderInd;
+	}
 
-	private Integer supplierNbr;
-	
-	private Price rtlPrice;
+	public void setIsCannedOrderInd(String isCannedOrderInd) {
+		this.isCannedOrderInd = isCannedOrderInd;
+	}
 
-	private List<String> countryOfOrigin;
+	public String getItemNbr() {
+		return itemNbr;
+	}
 
-	private Date createTs;
-	
-	private Date expirationDate;
-	
+	public void setItemNbr(String itemNbr) {
+		this.itemNbr = itemNbr;
+	}
+
+	public String getBaseDivisionCode() {
+		return baseDivisionCode;
+	}
+
+	public void setBaseDivisionCode(String baseDivisionCode) {
+		this.baseDivisionCode = baseDivisionCode;
+	}
+
+	public String getWarehouseMinLifeRemainingToReceiveQty() {
+		return warehouseMinLifeRemainingToReceiveQty;
+	}
+
+	public void setWarehouseMinLifeRemainingToReceiveQty(String warehouseMinLifeRemainingToReceiveQty) {
+		this.warehouseMinLifeRemainingToReceiveQty = warehouseMinLifeRemainingToReceiveQty;
+	}
+
+	public String getOmitTraitNbr() {
+		return omitTraitNbr;
+	}
+
+	public void setOmitTraitNbr(String omitTraitNbr) {
+		this.omitTraitNbr = omitTraitNbr;
+	}
+
+	public String getIsCancelWhenOutInd() {
+		return isCancelWhenOutInd;
+	}
+
+	public void setIsCancelWhenOutInd(String isCancelWhenOutInd) {
+		this.isCancelWhenOutInd = isCancelWhenOutInd;
+	}
+
+	public String getSupplierLeadTimeQty() {
+		return supplierLeadTimeQty;
+	}
+
+	public void setSupplierLeadTimeQty(String supplierLeadTimeQty) {
+		this.supplierLeadTimeQty = supplierLeadTimeQty;
+	}
+
+	public String getIsOfferedForSaleInd() {
+		return isOfferedForSaleInd;
+	}
+
+	public void setIsOfferedForSaleInd(String isOfferedForSaleInd) {
+		this.isOfferedForSaleInd = isOfferedForSaleInd;
+	}
+
+	public String getWarehouseAlignmentCode() {
+		return warehouseAlignmentCode;
+	}
+
+	public void setWarehouseAlignmentCode(String warehouseAlignmentCode) {
+		this.warehouseAlignmentCode = warehouseAlignmentCode;
+	}
+
+	public String getSeasonCode() {
+		return seasonCode;
+	}
+
+	public void setSeasonCode(String seasonCode) {
+		this.seasonCode = seasonCode;
+	}
+
+	public String getFactoryId() {
+		return factoryId;
+	}
+
+	public void setFactoryId(String factoryId) {
+		this.factoryId = factoryId;
+	}
+
+	public String getPublishedGtin() {
+		return publishedGtin;
+	}
+
+	public void setPublishedGtin(String publishedGtin) {
+		this.publishedGtin = publishedGtin;
+	}
+
+	public String getSeasonYearNbr() {
+		return seasonYearNbr;
+	}
+
+	public void setSeasonYearNbr(String seasonYearNbr) {
+		this.seasonYearNbr = seasonYearNbr;
+	}
+
+	public String getWarehousePackGtin() {
+		return warehousePackGtin;
+	}
+
+	public void setWarehousePackGtin(String warehousePackGtin) {
+		this.warehousePackGtin = warehousePackGtin;
+	}
+
+	public String getItemChangeSendWalmartWeekNbr() {
+		return itemChangeSendWalmartWeekNbr;
+	}
+
+	public void setItemChangeSendWalmartWeekNbr(String itemChangeSendWalmartWeekNbr) {
+		this.itemChangeSendWalmartWeekNbr = itemChangeSendWalmartWeekNbr;
+	}
+
+	public String getSupplierLeadTimeUomCode() {
+		return supplierLeadTimeUomCode;
+	}
+
+	public void setSupplierLeadTimeUomCode(String supplierLeadTimeUomCode) {
+		this.supplierLeadTimeUomCode = supplierLeadTimeUomCode;
+	}
+
+	public String getHasRfidInd() {
+		return hasRfidInd;
+	}
+
+	public void setHasRfidInd(String hasRfidInd) {
+		this.hasRfidInd = hasRfidInd;
+	}
+
+	public String getShelfLabel3Description() {
+		return shelfLabel3Description;
+	}
+
+	public void setShelfLabel3Description(String shelfLabel3Description) {
+		this.shelfLabel3Description = shelfLabel3Description;
+	}
+
+	public String getIsImportInd() {
+		return isImportInd;
+	}
+
+	public void setIsImportInd(String isImportInd) {
+		this.isImportInd = isImportInd;
+	}
+
+	public String getOrderablePackGtin() {
+		return orderablePackGtin;
+	}
+
+	public void setOrderablePackGtin(String orderablePackGtin) {
+		this.orderablePackGtin = orderablePackGtin;
+	}
+
+	public String getWarehouseAreaCode() {
+		return warehouseAreaCode;
+	}
+
+	public void setWarehouseAreaCode(String warehouseAreaCode) {
+		this.warehouseAreaCode = warehouseAreaCode;
+	}
+
+	public String getSupplyItemSecondaryDescription() {
+		return supplyItemSecondaryDescription;
+	}
+
+	public void setSupplyItemSecondaryDescription(String supplyItemSecondaryDescription) {
+		this.supplyItemSecondaryDescription = supplyItemSecondaryDescription;
+	}
+
+	public String getReserveMerchandiseTypeCode() {
+		return reserveMerchandiseTypeCode;
+	}
+
+	public void setReserveMerchandiseTypeCode(String reserveMerchandiseTypeCode) {
+		this.reserveMerchandiseTypeCode = reserveMerchandiseTypeCode;
+	}
+
+	public String getIsShelfLabelRequiredInd() {
+		return isShelfLabelRequiredInd;
+	}
+
+	public void setIsShelfLabelRequiredInd(String isShelfLabelRequiredInd) {
+		this.isShelfLabelRequiredInd = isShelfLabelRequiredInd;
+	}
+
+	public String getMerchandiseProgramId() {
+		return merchandiseProgramId;
+	}
+
+	public void setMerchandiseProgramId(String merchandiseProgramId) {
+		this.merchandiseProgramId = merchandiseProgramId;
+	}
+
+	public String getHasSecurityTagInd() {
+		return hasSecurityTagInd;
+	}
+
+	public void setHasSecurityTagInd(String hasSecurityTagInd) {
+		this.hasSecurityTagInd = hasSecurityTagInd;
+	}
+
+	public String getSupplyItemChangeReasonCode() {
+		return supplyItemChangeReasonCode;
+	}
+
+	public void setSupplyItemChangeReasonCode(String supplyItemChangeReasonCode) {
+		this.supplyItemChangeReasonCode = supplyItemChangeReasonCode;
+	}
+
+	public String getAllowedTimeInWarehouseUomCode() {
+		return allowedTimeInWarehouseUomCode;
+	}
+
+	public void setAllowedTimeInWarehouseUomCode(String allowedTimeInWarehouseUomCode) {
+		this.allowedTimeInWarehouseUomCode = allowedTimeInWarehouseUomCode;
+	}
+
+	public String getWarehouseMaxOrderQty() {
+		return warehouseMaxOrderQty;
+	}
+
+	public void setWarehouseMaxOrderQty(String warehouseMaxOrderQty) {
+		this.warehouseMaxOrderQty = warehouseMaxOrderQty;
+	}
+
+	public List<String> getDestinationFormatCode() {
+		return destinationFormatCode;
+	}
+
+	public void setDestinationFormatCode(List<String> destinationFormatCode) {
+		this.destinationFormatCode = destinationFormatCode;
+	}
+
+	public String getWarehousePackQtyUomCode() {
+		return warehousePackQtyUomCode;
+	}
+
+	public void setWarehousePackQtyUomCode(String warehousePackQtyUomCode) {
+		this.warehousePackQtyUomCode = warehousePackQtyUomCode;
+	}
+
+	public String getLegacyProductNbr() {
+		return legacyProductNbr;
+	}
+
+	public void setLegacyProductNbr(String legacyProductNbr) {
+		this.legacyProductNbr = legacyProductNbr;
+	}
+
+	public String getAllowedTimeInWarehouseQty() {
+		return allowedTimeInWarehouseQty;
+	}
+
+	public void setAllowedTimeInWarehouseQty(String allowedTimeInWarehouseQty) {
+		this.allowedTimeInWarehouseQty = allowedTimeInWarehouseQty;
+	}
+
+	public String getIsReplenishedByUnitInd() {
+		return isReplenishedByUnitInd;
+	}
+
+	public void setIsReplenishedByUnitInd(String isReplenishedByUnitInd) {
+		this.isReplenishedByUnitInd = isReplenishedByUnitInd;
+	}
+
+	public String getMbmTypeCode() {
+		return mbmTypeCode;
+	}
+
+	public void setMbmTypeCode(String mbmTypeCode) {
+		this.mbmTypeCode = mbmTypeCode;
+	}
+
+	public String getSupplierId() {
+		return supplierId;
+	}
+
+	public void setSupplierId(String supplierId) {
+		this.supplierId = supplierId;
+	}
+
+	public String getIsCorporateReplenishableInd() {
+		return isCorporateReplenishableInd;
+	}
+
+	public void setIsCorporateReplenishableInd(String isCorporateReplenishableInd) {
+		this.isCorporateReplenishableInd = isCorporateReplenishableInd;
+	}
+
+	public String getIsEcommerceReplenishableInd() {
+		return isEcommerceReplenishableInd;
+	}
+
+	public void setIsEcommerceReplenishableInd(String isEcommerceReplenishableInd) {
+		this.isEcommerceReplenishableInd = isEcommerceReplenishableInd;
+	}
+
+	public String getWarehousePackCalcMethodCode() {
+		return warehousePackCalcMethodCode;
+	}
+
+	public void setWarehousePackCalcMethodCode(String warehousePackCalcMethodCode) {
+		this.warehousePackCalcMethodCode = warehousePackCalcMethodCode;
+	}
+
+	public String getRecipientGln() {
+		return recipientGln;
+	}
+
+	public void setRecipientGln(String recipientGln) {
+		this.recipientGln = recipientGln;
+	}
+
+	public String getWarehousePackCostAmt() {
+		return warehousePackCostAmt;
+	}
+
+	public void setWarehousePackCostAmt(String warehousePackCostAmt) {
+		this.warehousePackCostAmt = warehousePackCostAmt;
+	}
+
+	public String getDeptNbr() {
+		return deptNbr;
+	}
+
+	public void setDeptNbr(String deptNbr) {
+		this.deptNbr = deptNbr;
+	}
+
+	public String getSupplyItemPrimaryDescription() {
+		return supplyItemPrimaryDescription;
+	}
+
+	public void setSupplyItemPrimaryDescription(String supplyItemPrimaryDescription) {
+		this.supplyItemPrimaryDescription = supplyItemPrimaryDescription;
+	}
+
+	public String getBuyingRegionCode() {
+		return buyingRegionCode;
+	}
+
+	public void setBuyingRegionCode(String buyingRegionCode) {
+		this.buyingRegionCode = buyingRegionCode;
+	}
+
+	public Date getSupplyItemEffectiveDate() {
+		return supplyItemEffectiveDate;
+	}
+
+	public void setSupplyItemEffectiveDate(Date supplyItemEffectiveDate) {
+		this.supplyItemEffectiveDate = supplyItemEffectiveDate;
+	}
+
+	public List<String> getOriginCountryCode() {
+		return originCountryCode;
+	}
+
+	public void setOriginCountryCode(List<String> originCountryCode) {
+		this.originCountryCode = originCountryCode;
+	}
+
+	public String getSupplyItemStatusCode() {
+		return supplyItemStatusCode;
+	}
+
+	public void setSupplyItemStatusCode(String supplyItemStatusCode) {
+		this.supplyItemStatusCode = supplyItemStatusCode;
+	}
+
+	public String getInformationProviderId() {
+		return informationProviderId;
+	}
+
+	public void setInformationProviderId(String informationProviderId) {
+		this.informationProviderId = informationProviderId;
+	}
+
 	public String getConsumableGtin() {
 		return consumableGtin;
 	}
@@ -38,68 +478,148 @@ public class LogisticalItem implements Serializable {
 		this.consumableGtin = consumableGtin;
 	}
 
-	public String getSupplierGtin() {
-		return supplierGtin;
+	public String getSupplierStockId() {
+		return supplierStockId;
 	}
 
-	public void setSupplierGtin(String supplierGtin) {
-		this.supplierGtin = supplierGtin;
+	public void setSupplierStockId(String supplierStockId) {
+		this.supplierStockId = supplierStockId;
 	}
 
-	public String getConsumableGtinId() {
-		return consumableGtinId;
+	public String getBaseRetailUomCode() {
+		return baseRetailUomCode;
 	}
 
-	public void setConsumableGtinId(String consumableGtinId) {
-		this.consumableGtinId = consumableGtinId;
+	public void setBaseRetailUomCode(String baseRetailUomCode) {
+		this.baseRetailUomCode = baseRetailUomCode;
 	}
 
-	public String getSupplierGtinId() {
-		return supplierGtinId;
+	public String getIsVariableWeightInd() {
+		return isVariableWeightInd;
 	}
 
-	public void setSupplierGtinId(String supplierGtinId) {
-		this.supplierGtinId = supplierGtinId;
+	public void setIsVariableWeightInd(String isVariableWeightInd) {
+		this.isVariableWeightInd = isVariableWeightInd;
 	}
 
-	public Integer getSupplierNbr() {
-		return supplierNbr;
+	public String getOrderablePackCostAmt() {
+		return orderablePackCostAmt;
 	}
 
-	public void setSupplierNbr(Integer supplierNbr) {
-		this.supplierNbr = supplierNbr;
+	public void setOrderablePackCostAmt(String orderablePackCostAmt) {
+		this.orderablePackCostAmt = orderablePackCostAmt;
 	}
 
-	public List<String> getCountryOfOrigin() {
-		return countryOfOrigin;
+	public String getReplenishSubTypeCode() {
+		return replenishSubTypeCode;
 	}
 
-	public void setCountryOfOrigin(List<String> countryOfOrigin) {
-		this.countryOfOrigin = countryOfOrigin;
+	public void setReplenishSubTypeCode(String replenishSubTypeCode) {
+		this.replenishSubTypeCode = replenishSubTypeCode;
 	}
 
-	public Date getCreateTs() {
-		return createTs;
+	public String getWarehousePackQty() {
+		return warehousePackQty;
 	}
 
-	public void setCreateTs(Date createTs) {
-		this.createTs = createTs;
+	public void setWarehousePackQty(String warehousePackQty) {
+		this.warehousePackQty = warehousePackQty;
 	}
 
-	public Date getExpirationDate() {
-		return expirationDate;
+	public String getInformationProviderTypeCode() {
+		return informationProviderTypeCode;
 	}
 
-	public void setExpirationDate(Date expirationDate) {
-		this.expirationDate = expirationDate;
+	public void setInformationProviderTypeCode(String informationProviderTypeCode) {
+		this.informationProviderTypeCode = informationProviderTypeCode;
 	}
 
-	public Price getRtlPrice() {
-		return rtlPrice;
+	public String getItemTypeCode() {
+		return itemTypeCode;
 	}
 
-	public void setRtlPrice(Price rtlPrice) {
-		this.rtlPrice = rtlPrice;
+	public void setItemTypeCode(String itemTypeCode) {
+		this.itemTypeCode = itemTypeCode;
+	}
+
+	public String getWarehouseRotationTypeCode() {
+		return warehouseRotationTypeCode;
+	}
+
+	public void setWarehouseRotationTypeCode(String warehouseRotationTypeCode) {
+		this.warehouseRotationTypeCode = warehouseRotationTypeCode;
+	}
+
+	public String getOrderablePackQty() {
+		return orderablePackQty;
+	}
+
+	public void setOrderablePackQty(String orderablePackQty) {
+		this.orderablePackQty = orderablePackQty;
+	}
+
+	public String getTargetMarketCode() {
+		return targetMarketCode;
+	}
+
+	public void setTargetMarketCode(String targetMarketCode) {
+		this.targetMarketCode = targetMarketCode;
+	}
+
+	public Date getSendStoreDate() {
+		return sendStoreDate;
+	}
+
+	public void setSendStoreDate(Date sendStoreDate) {
+		this.sendStoreDate = sendStoreDate;
+	}
+
+	public String getHasRigidPlasticPackagingContainerInd() {
+		return hasRigidPlasticPackagingContainerInd;
+	}
+
+	public void setHasRigidPlasticPackagingContainerInd(String hasRigidPlasticPackagingContainerInd) {
+		this.hasRigidPlasticPackagingContainerInd = hasRigidPlasticPackagingContainerInd;
+	}
+
+	public String getUnitCostAmt() {
+		return unitCostAmt;
+	}
+
+	public void setUnitCostAmt(String unitCostAmt) {
+		this.unitCostAmt = unitCostAmt;
+	}
+
+	public String getBaseRetailAmt() {
+		return baseRetailAmt;
+	}
+
+	public void setBaseRetailAmt(String baseRetailAmt) {
+		this.baseRetailAmt = baseRetailAmt;
+	}
+
+	public String getFinelineNbr() {
+		return finelineNbr;
+	}
+
+	public void setFinelineNbr(String finelineNbr) {
+		this.finelineNbr = finelineNbr;
+	}
+
+	public String getIsBackroomScaleInd() {
+		return isBackroomScaleInd;
+	}
+
+	public void setIsBackroomScaleInd(String isBackroomScaleInd) {
+		this.isBackroomScaleInd = isBackroomScaleInd;
+	}
+
+	public Date getSupplyItemExpireDate() {
+		return supplyItemExpireDate;
+	}
+
+	public void setSupplyItemExpireDate(Date supplyItemExpireDate) {
+		this.supplyItemExpireDate = supplyItemExpireDate;
 	}
 
 	public String get_id() {
--- supply-chain-services/src/main/java/com/ust/supplychain/initializer/DataInitializer.java
+++ supply-chain-services/src/main/java/com/ust/supplychain/initializer/DataInitializer.java
@@ -1,7 +1,13 @@
 package com.ust.supplychain.initializer;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Iterator;
+import java.util.List;
 
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -10,6 +16,7 @@ import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.jongo.Jongo;
+import org.jongo.MongoCursor;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
@@ -35,34 +42,64 @@ public class DataInitializer {
 	@Inject
 	private ObjectMapper mapper;
 
-	public static void main(String[] args) throws JsonParseException, JsonMappingException, JSONException, IOException {
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, JSONException, IOException, URISyntaxException {
 		Injector injector = Guice.createInjector(new BootstrapPropertiesModule());
 		DataInitializer intializer = injector.getInstance(DataInitializer.class);
 		
 //		intializer.jongo.getCollection("metadata").drop();
 //		intializer.loadProdMetadata();
 //		intializer.loadTIMetadata();
-//		intializer.loadLogisticalItems();
 //		intializer.loadTradeItems();
 //		intializer.loadProducts();
-		intializer.loadLogisticalItemsMetadata();
+//		intializer.loadLogisticalItemsMetadata();
+		intializer.loadLogisticalItems();
 	}
 
-	private void loadLogisticalItemsMetadata() throws JSONException, IOException {
+	private void loadLogisticalItemsMetadata() throws JSONException, IOException, URISyntaxException {
 		JSONObject prods = new JSONObject(Resources.toString(Resources.getResource("logistical-items-metadata.json"), Charsets.UTF_8));
 		JSONObject optAttrs = prods.getJSONObject("attributes").getJSONObject("Optional");
 		JSONObject reqAttrs = prods.getJSONObject("attributes").getJSONObject("Required");
 		Iterator<String> optKeys = optAttrs.keys();
 		Iterator<String> reqKeys = reqAttrs.keys();
+		JSONObject aSI = new JSONObject(); // To create Data set
+		JSONObject attrInfo = null;
 		while(optKeys.hasNext()){
-			JSONObject optMeta = optAttrs.getJSONObject(optKeys.next()).getJSONObject("metadata");
+			attrInfo = optAttrs.getJSONObject(optKeys.next());
+			JSONObject optMeta = attrInfo.getJSONObject("metadata");
 			jongo.getCollection("metadata").save(mapper.readValue(optMeta.toString(), Metadata.class));
+			
+			aSI.put(attrInfo.getJSONObject("metadata").getString("id"), getAttrValue(attrInfo));
 		}
 		while(reqKeys.hasNext()){
-			JSONObject reqMeta = reqAttrs.getJSONObject(reqKeys.next()).getJSONObject("metadata");
+			attrInfo = reqAttrs.getJSONObject(reqKeys.next());
+			JSONObject reqMeta = attrInfo.getJSONObject("metadata");
 			jongo.getCollection("metadata").save(mapper.readValue(reqMeta.toString(), Metadata.class));
+			aSI.put(attrInfo.getJSONObject("metadata").getString("id"), getAttrValue(attrInfo));
 		}
+		aSI.put("_id", (aSI.getString("targetMarketCode") + aSI.getString("baseDivisionCode") + aSI.getString("itemNbr")).toUpperCase());
+		JSONArray allSIs = new JSONArray(Resources.toString(Resources.getResource("logistical-items.json"), Charsets.UTF_8));
+		allSIs.put(aSI);
+		writeToFile(allSIs.toString(), "file:/C:/Users/u17078/mongo-poc/supply-chain-services/src/main/resources/logistical-items.json");
 		
+	}
+
+	private void writeToFile(String content, String filePath) throws IOException, URISyntaxException {
+		 Files.write(Paths.get(new URI(filePath)), content.getBytes(), StandardOpenOption.CREATE);
+	}
+
+	private Object getAttrValue(JSONObject attrInfo) throws JSONException {
+		JSONObject values = attrInfo.optJSONObject("values");
+		if(values == null) {
+			return null;
+		}
+		JSONArray valueList = values.getJSONArray("valuesList");
+		if(valueList == null || valueList.length()  == 0) {
+			return null;
+		} 
+		else if(valueList.length()  == 1) {
+			return valueList.get(0);
+		}
+		return valueList;
 	}
 
 	private void loadTIMetadata() throws JSONException, IOException {
@@ -128,8 +165,8 @@ public class DataInitializer {
 			jongo.getCollection(collectionName).save(item);
 		}
 //		To QUERY
-//		LogisticalItem item1 = jongo.getCollection(collectionName).findOne("{consumableGtinId: '0000000000017_GLN_000000009_US_WM'}").as(LogisticalItem.class);
-		System.out.println("Queried");
+		MongoCursor<List> o = jongo.getCollection(collectionName).find("{consumableGtin: '00010512000122'}").as(List.class);
+		System.out.println("Queried" + o.count());
 	}
 
 }
